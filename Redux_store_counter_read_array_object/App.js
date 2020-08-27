ba40dec9062f546f5e590decf30cbdf86cbe280f
arulvee <arulvee@users.noreply.github.com>
2020-08-26 22:04:31 -0700

    Create App.js

---
+++ Redux_store_counter_read_array_object/App.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react";
+import "./style.css";
+
+import store, {COUNTER_INCREMENT, COUNTER_DECREMENT, GET_VALUES} from './stores';
+
+const increment = () => {
+  store.dispatch({type:COUNTER_INCREMENT});
+  console.log(store.getState());
+}
+const decrement = () => {
+  store.dispatch({type:COUNTER_DECREMENT});
+  console.log(store.getState())
+}
+//No Hooks
+/*const getValues = () => {
+  store.dispatch({type:GET_VALUES});
+  console.log(...store.getState());
+}*/
+//With Hooks
+export default function App() {
+  useEffect (() => {
+  store.dispatch({type:GET_VALUES});
+  console.log(...store.getState());
+},[])
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      {/* <button onClick={getValues}>Get Values</button> */}
+      
+    </div>
+  );
+}
